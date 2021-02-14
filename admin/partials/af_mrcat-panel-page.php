bc353ff0a9e68c6adacaca53a5d1148f8e9a606f
hossein shourabi (MrCatDev) <hosein.shorabi@live.com>
2021-02-14 12:31:06 +0200

    edit url

--- admin/partials/af_mrcat-panel-page.php
+++ admin/partials/af_mrcat-panel-page.php
@@ -8,7 +8,7 @@
     <?php
         settings_fields('af_mrcat_setting');
         do_settings_sections('af_mrcat_setting');
-        $image = get_option('af_mrcat_logo') ?? 'https://via.placeholder.com/300/555555/FFFFFF/?text=Upload%20your%20logo';
+        $image = get_option('af_mrcat_logo') ? get_option('af_mrcat_logo') : 'https://via.placeholder.com/300/555555/FFFFFF/?text=Upload%20your%20logo';
     ?>
 
     <input type="text" name="af_mrcat_text" value="<?php echo get_option('af_mrcat_text'); ?>" placehoder="Add your text here" />
